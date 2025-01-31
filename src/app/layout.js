f5b94af7c5617fb267560c4b634f4d5dbcb61a6f
arif <arifk62901@gmail.com>
2025-01-31 20:38:53 +0600

    Added to font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Home from "./page";
-import Homepage from "./Home/Homepage";
+
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+
+const poppins = Poppins({
+  weight:["400","600","700"],
+  subsets:["latin"]
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -18,8 +23,10 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title:{
     default:"Rescue companion app",
-    template:"%s  | Rescue companion app "
+    template: `%s  | Rescue companion app `
   },
+  
+  keywords: ['Next.js', 'React', 'JavaScript','Learn to next js','typescript','playground'],
   description: "Trying to learn about Next js  ",
 };
 
@@ -27,7 +34,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme='light'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <Navbar />
            
